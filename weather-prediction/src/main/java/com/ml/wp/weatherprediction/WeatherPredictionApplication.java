73b0ac115ae1d3444a62e225d728f3f6da6ea33b
mauritoBona <maurobonanno6@gmail.com>
2018-06-13 23:59:22 -0300

    Initial Commit

---
+++ weather-prediction/src/main/java/com/ml/wp/weatherprediction/WeatherPredictionApplication.java
@@ -0,0 +1,12 @@
+package com.ml.wp.weatherprediction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherPredictionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeatherPredictionApplication.class, args);
+	}
+}
